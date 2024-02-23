01535cdede3035389cd3b880e05a8d4053834ecf
Vladyslav Zadorozhnyy <zadorozhnyy@ucu.edu.ua>
2024-02-23 14:47:47 +0200

    Refactored some MealDetailsScreenFragment screen.

--- app/src/main/java/com/bitebalance/presentation/ui/fragments/MenuScreenFragment.kt
+++ app/src/main/java/com/bitebalance/presentation/ui/fragments/MenuScreenFragment.kt
@@ -1,76 +1,71 @@
 package com.bitebalance.presentation.ui.fragments
 
-import android.content.res.ColorStateList
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.ui.components.R
+import com.ui.model.DishModel
+import com.ui.common.Constants
+import android.content.res.ColorStateList
+import com.ui.basic.texts.common.TextModel
 import com.bitebalance.common.NavigationAction
+import com.ui.basic.buttons.common.ButtonModel
+import com.ui.components.databinding.ToolbarBinding
+import com.ui.components.databinding.NoItemsLayoutBinding
 import com.bitebalance.databinding.FragmentMenuScreenBinding
 import com.bitebalance.presentation.viewmodels.DishViewModel
-import com.bitebalance.presentation.viewmodels.NavigationViewModel
-import com.bitebalance.presentation.viewmodels.ThemeViewModel
-import com.ui.basic.buttons.common.ButtonModel
-import com.ui.basic.recycler_views.dish_recycler.DishRecyclerModel
-import com.ui.basic.texts.common.TextModel
-import com.ui.components.R
-import com.ui.components.databinding.NoItemsLayoutBinding
-import com.ui.model.DishModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import com.ui.basic.recycler_views.dish_recycler.DishRecyclerModel
 
-class MenuScreenFragment : Fragment() {
-    private val binding by lazy { FragmentMenuScreenBinding.inflate(layoutInflater) }
-    private val noItemsLayoutBinding by lazy { NoItemsLayoutBinding.bind(binding.root) }
-
-    private val navigationVm by sharedViewModel<NavigationViewModel>()
-    private val themeViewModel by sharedViewModel<ThemeViewModel>()
-
+class MenuScreenFragment : BaseFragment<FragmentMenuScreenBinding>() {
     private val dishVm by sharedViewModel<DishViewModel>()
-    private var creatingNewMeal = false
+    private var createNewMeal = false
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        setupViewModelsObservation()
+    override fun onStartFragment(): View {
+        binding = FragmentMenuScreenBinding.inflate(layoutInflater)
+        noItemsLayoutBinding = NoItemsLayoutBinding.bind(binding.root)
+        toolbarBinding = ToolbarBinding.bind(binding.sublayoutContainer)
+
         return binding.root
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onResumeFragment() {
+        super.onResumeFragment()
         dishVm.getAllDishes()
     }
 
-    private fun setupViewModelsObservation() {
+    override fun setupViewModelsObservation() {
         dishVm.state.observe(this) { state ->
             if (state.data == null) { return@observe }
             if (state.data.isEmpty()) { setupNoItemsView() } else { setupDishRecycler(state.data) }
         }
-
-        themeViewModel.state.observe(this) {
+        themeVm.state.observe(this) {
             setupHeader()
             setupStyling()
             setupCreateNewButton()
         }
     }
 
-    private fun setupHeader() {
-        binding.toolbar.backButton.visibility = View.GONE
-        binding.toolbar.forwardButton.visibility = View.GONE
+    override fun onStopFragment() {
+        dishVm.resetState()
+        dishVm.state.removeObservers(this)
+        themeVm.state.removeObservers(this)
+        navigationVm.state.removeObservers(this)
+    }
 
-        binding.toolbar.headline.setup(
+    private fun setupHeader() {
+        toolbarBinding.backButton.visibility = View.GONE
+        toolbarBinding.forwardButton.visibility = View.GONE
+        toolbarBinding.headline.setup(
             model = TextModel(
-                textValue = "Menu",
-                textSize = 30,
-                textColor = themeViewModel.state.value!!.primaryColor,
-                backgroundColor = themeViewModel.state.value!!.secondaryColor,
-            )
+                textValue = requireContext().getString(R.string.menu),
+                textSize = Constants.TEXT_SIZE_BIG,
+                textColor = themeVm.state.value!!.primaryColor,
+                backgroundColor = themeVm.state.value!!.secondaryColor,
+            ),
         )
     }
 
     private fun setupStyling() {
-        binding.root.setBackgroundColor(themeViewModel.state.value!!.secondaryColor)
+        binding.root.setBackgroundColor(themeVm.state.value!!.secondaryColor)
     }
 
     private fun setupDishRecycler(dishItems: List<DishModel>) {
@@ -81,10 +76,10 @@ class MenuScreenFragment : Fragment() {
         binding.dishRecycler.setup(
             model = DishRecyclerModel(
                 items = dishItems,
-                primaryColor = themeViewModel.state.value!!.primaryColor,
-                secondaryColor = themeViewModel.state.value!!.secondaryColor,
-                onClickListener = { processDishClick(it) }
-            )
+                primaryColor = themeVm.state.value!!.primaryColor,
+                secondaryColor = themeVm.state.value!!.secondaryColor,
+                onClickListener = { processDishClick(it) },
+            ),
         )
     }
 
@@ -92,19 +87,19 @@ class MenuScreenFragment : Fragment() {
         binding.createNewMealButton.setup(
             model = ButtonModel(
                 iconRes = R.drawable.add_icon,
-                iconSize = 80,
-                strokeWidth = 5,
-                labelTextSize = 14,
+                iconSize = Constants.ICON_SIZE_MEDIUM,
+                strokeWidth = Constants.COLOR_ICON_STROKE_WIDTH,
+                labelTextSize = Constants.TEXT_SIZE_SMALL,
                 labelTextRes = R.string.add_new,
-                foregroundColor = themeViewModel.state.value!!.primaryColor,
-                backgroundColor = themeViewModel.state.value!!.secondaryColor,
+                foregroundColor = themeVm.state.value!!.primaryColor,
+                backgroundColor = themeVm.state.value!!.secondaryColor,
                 onClickListener =  {
                     navigationVm.navigateTo(
                         CreateNewScreenFragment.newInstance(createDish = true),
                         NavigationAction.ADD
                     )
-                }
-            )
+                },
+            ),
         )
     }
 
@@ -114,35 +109,27 @@ class MenuScreenFragment : Fragment() {
         binding.dishRecycler.visibility = View.INVISIBLE
 
         noItemsLayoutBinding.imageView.setBackgroundResource(R.drawable.empty_menu_icon)
-        noItemsLayoutBinding.imageView.backgroundTintList = ColorStateList.valueOf(themeViewModel.state.value!!.primaryColor)
+        noItemsLayoutBinding.imageView.backgroundTintList = ColorStateList.valueOf(themeVm.state.value!!.primaryColor)
         noItemsLayoutBinding.messageView.setup(
             model = TextModel(
-                textValue = "Seems that you have no dishes yet. \n Start by adding one.",
-                textSize = 25,
-                textColor = themeViewModel.state.value!!.primaryColor,
-                backgroundColor = themeViewModel.state.value!!.secondaryColor,
-            )
+                textValue = requireContext().getString(R.string.no_dishes_yet),
+                textSize = Constants.TEXT_SIZE_BIG,
+                textColor = themeVm.state.value!!.primaryColor,
+                backgroundColor = themeVm.state.value!!.secondaryColor,
+            ),
         )
     }
 
     private fun processDishClick(dish: DishModel) {
-        if (creatingNewMeal) {
+        if (createNewMeal)
             navigationVm.navigateTo(CreateNewScreenFragment.newInstance(createDish = false), NavigationAction.ADD)
-        } else {
+        else
             navigationVm.navigateTo(DishScreenFragment.newInstance(dish.name), NavigationAction.ADD)
-        }
-    }
-
-    override fun onDestroy() {
-        dishVm.resetState()
-        dishVm.state.removeObservers(this)
-        navigationVm.state.removeObservers(this)
-        super.onDestroy()
     }
 
     companion object {
         fun newInstance(creatingNewMeal: Boolean): MenuScreenFragment {
-            return MenuScreenFragment().also { it.creatingNewMeal = creatingNewMeal }
+            return MenuScreenFragment().also { it.createNewMeal = creatingNewMeal }
         }
     }
 }
