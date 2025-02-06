1797e32f0f3b4ab56a441cd79d8d43ad742ea133
VladyslavZadorozhnyy <vladyslavzadorozhnyy19@gmail.com>
2025-02-06 14:58:37 +0200

    [MA-8] Create unit tests with Junit4. Modified Category test runner.

---
+++ app/src/test/java/com/bitebalance/unit/junit4/EnclosedUnitTest.kt
@@ -0,0 +1,32 @@
+package com.bitebalance.unit.junit4
+
+import com.ui.basic.recycler_views.text_recycler.TextAdapter
+import com.ui.basic.recycler_views.text_recycler.TextRecyclerModel
+import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.runner.RunWith
+
+@RunWith(Enclosed::class)
+class EnclosedUnitTest {
+    class TextRecyclerModelUnitTest {
+        @Test
+        fun test() {
+            println("TextRecyclerModelUnitTest, test()")
+            TextRecyclerModel(
+                items = listOf(),
+                onClickListener = {},
+            )
+        }
+    }
+
+    class TextAdapterUnitTest {
+        @Test
+        fun test() {
+            println("TextAdapterUnitTest, test()")
+            TextAdapter(
+                items = listOf(),
+                onClickListener = {}
+            )
+        }
+    }
+}
