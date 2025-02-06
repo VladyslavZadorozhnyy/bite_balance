6d66db516d4a64502941c7c8432e2d3f42b0a2c2
VladyslavZadorozhnyy <vladyslavzadorozhnyy19@gmail.com>
2025-02-06 14:32:28 +0200

    [MA-8] Create unit tests with Junit4. Modified Category test runner.

--- app/src/test/java/com/bitebalance/unit/junit4/MetricUnitTest.kt
+++ app/src/test/java/com/bitebalance/unit/junit4/MetricUnitTest.kt
@@ -5,11 +5,11 @@ import com.ui.basic.recycler_views.metric_recycler.MetricRecyclerModel
 import org.junit.Test
 import org.junit.experimental.categories.Category
 
-@Category(MetricTests::class)
 class MetricUnitTest {
+    @Category(MetricTests::class)
     @Test
     fun test_metric_recycler_model() {
-        println("test_metric_recycler_model")
+        println("test_metric_recycler_model, category")
         MetricRecyclerModel(
             items = listOf(),
             foregroundColor = Color.BLACK,
--- app/src/test/java/com/bitebalance/unit/junit4/SettingsUnitTest.kt
+++ app/src/test/java/com/bitebalance/unit/junit4/SettingsUnitTest.kt
@@ -5,11 +5,11 @@ import com.ui.basic.recycler_views.settings_recycler.SettingsRecyclerModel
 import org.junit.Test
 import org.junit.experimental.categories.Category
 
-@Category(SettingsTests::class)
 class SettingsUnitTest {
+    @Category(SettingsTests::class)
     @Test
     fun settings_recycler_model_test() {
-        println("settings_recycler_model_test")
+        println("settings_recycler_model_test, category")
         SettingsRecyclerModel(
             items = listOf(),
             primaryColor = Color.WHITE,
