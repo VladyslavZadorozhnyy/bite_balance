608da4d160fe899104ed59ea76f91a411cceda9b
VladyslavZadorozhnyy <vladyslavzadorozhnyy19@gmail.com>
2025-02-06 18:16:44 +0200

    [MA-8] Create unit tests with Junit4. Created AndroidJUnit4 test runner.

---
+++ app/src/androidTest/java/com/bitebalance/AndroidJUnit4UnitTest.kt
@@ -0,0 +1,18 @@
+package com.bitebalance
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+
+@RunWith(AndroidJUnit4::class)
+class AndroidJUnit4UnitTest {
+    @Test
+    fun test_component(componentName: String) {
+//        println("ApplicationProvider.getApplicationContext<Context>(): ${ApplicationProvider.getApplicationContext<Context>()}")
+        assert(true)
+    }
+}
